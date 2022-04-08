07f7bb769191382e84aa62b12a83f158e3ef47b4
Harmela <seifuharmela@gmail.com>
2022-04-07 19:07:42 -0700

    updated script and removed bug in decimal button

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
+
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator-keys');
 const display = document.querySelector('.calculator-display');
+
+
+const operate = (n1, operator, n2) => {
+    let result = '';
+    // calculator.dataset.previousKeyType = "operate";
+    if (operator === "add") {
+        // result = parseFloat(n1) + parseFloat(n2);
+        result = add(n1, n2);
+    } else if (operator === "subtract"){
+        // result = parseFloat(n1) - parseFloat(n2);
+        result = subtract(n1,n2);
+    } else if (operator === "multiply"){
+        // result = parseFloat(n1) * parseFloat(n2);
+        result = multiply(n1, n2);
+    } else if (operator === "divide"){
+        // result = parseFloat(n1) / parseFloat(n2);
+        if(n2 === "0"){
+          result = "error";
+        } else{
+            result = divide(n1, n2);
+
+        }
+    }
+    return result;
+}
+
+function add(a,b) {  
+    return parseFloat(a) + parseFloat(b);
+} 
+
+function subtract(a,b) {  
+    return parseFloat(a) - parseFloat(b);
+} 
+
+function multiply(a,b) {  
+    return parseFloat(a) * parseFloat(b);
+} 
+
+function divide(a,b) {  
+    return parseFloat(a) / parseFloat(b);
+} 
+
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
@@ -11,181 +54,97 @@ keys.addEventListener('click', e => {
         const displayNumber = display.textContent;
         const previousKeyType = calculator.dataset.previousKeyType;
 
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressedButton'));
+
         if (!action){
             console.log("number key");
-            if (displayNumber === "0" || previousKeyType === 'operator'){
+            if (displayNumber === "0" || previousKeyType === 'operator' || previousKeyType === 'calculate'){
                 display.textContent = keyContent;
             } else {
                 display.textContent = displayNumber + keyContent;
             }
-            // calculator.dataset.previousKey = "number";
+            calculator.dataset.previousKeyType = "number";
         }
+
+        if (action === "decimal"){
+            // if there is no decimal displayed, add one when button is clicked
+            if (!displayNumber.includes('.')) {
+                display.textContent = displayNumber + "."
+            } else if (previousKeyType === 'operator' || previousKeyType === 'calculate'){
+                display.textContent = '0.'
+            }
+            calculator.dataset.previousKeyType = "decimal";
+        }
+
         if (
             action === "add" ||
             action === "subtract" ||
             action === "multiply" ||
             action === "divide"
         ) {
-            console.log("operator key");
-            key.classList.add('pressedButton');
-            // add custom attribute
-            calculator.dataset.previousKeyType = 'operator';
-            calculator.dataset.firstValue = displayNumber;
-            calculator.dataset.operator = action;
-        }
-                    
-        if (action === "decimal"){
-            console.log("decimal key");
-            // if there is no decimal displayed, add one when button is clicked
-            if (!displayNumber.includes('.')) {
-                display.textContent = displayNumber + "."
-            } else if (previousKeyType === 'operator'){
-                display.textContent = '0.'
-            }
-            // calculator.dataset.previousKey = "decimal";
-        }
-        if (action === "clear"){
-            console.log("clear key")
-            display.textContent = "";
-            // calculator.dataset.previousKeyType = "clear";
-
-        }
-
-        const operate = (n1, operator, n2) => {
-            let result = '';
-            // calculator.dataset.previousKeyType = "operate";
-            if (operator === "add") {
-                result = parseFloat(n1) + parseFloat(n2);;
-            } else if (operator === "subtract"){
-                result = parseFloat(n1) - parseFloat(n2);;
-            } else if (operator === "multiply"){
-                result = parseFloat(n1) * parseFloat(n2);;
-            } else if (operator === "divide"){
-                result = parseFloat(n1) / parseFloat(n2);
-            }
-            console.log(result);
-            return result;
-
-        }
-
-        function add(a,b) {  
-            return parseFloat(a) + parseFloat(b);
-        } 
-
-        function subtract(a,b) {  
-            return parseFloat(a) - parseFloat(b);
-        } 
-
-        function multiply(a,b) {  
-            return parseFloat(a) * parseFloat(b);
-        } 
-
-        function divide(a,b) {  
-            return parseFloat(a) / parseFloat(b);
-        } 
-
-        if (action === "calculate"){
-            console.log("equal key")
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayNumber;
-            display.textContent = operate(firstValue, operator, secondValue)
+
+            // check if firstValue and operator exists because secondValue always exists
+            // if a user hits a number, operator, number, then another operator - display updates to calculated value
+            if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
+                
+                const calcValue = operate(firstValue, operator, secondValue);
+                display.textContent = calcValue;
+                // calculated value becomes first value when making multiple equations
+                calculator.dataset.firstValue = calcValue;
+                // display.textContent = operate(firstValue, operator, secondValue);
+            } else {
+                calculator.dataset.firstValue = displayNumber;
+            }
+
+            key.classList.add('pressedButton');
+            // add custom attribute
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.operator = action;
+        }
+                    
+        if (action === "clear"){
+            if (key.textContent === "AC"){
+                calculator.dataset.firstValue = '';
+                calculator.dataset.modValue = '';
+                calculator.dataset.operator = '';
+                calculator.dataset.previousKeyType = '';
+            } else {
+                key.textContent = "AC";
+            }
+
+            display.textContent = 0;
+            calculator.dataset.previousKeyType = "clear";
         }
 
+        // when a number/operand is clicked, the AC text on button is changed to CE
+        if (action !== 'clear'){
+            const clearButton = calculator.querySelector('.clearButton');
+            clearButton.textContent = 'CE';
+        }
 
-
-        Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressedButton'));
+        if (action === "calculate"){
+            let firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            let secondValue = displayNumber;
+           
+            if (firstValue){
+                if (previousKeyType === 'calculate'){
+                    firstValue = displayNumber;
+                    secondValue = calculator.dataset.modValue;
+                }
+                display.textContent = operate(firstValue, operator, secondValue)
+            }
+           
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = "calculate";
+        }
     }
 })
 
 
 
 
-
-
-
-// const calculator = document.querySelector('.calculator-container');
-// const display = document.querySelector('.display');
-// const buttons = document.querySelectorAll('button');
-// const previousKeyType = calculator.dataset.previousKeyType;
-
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (e) =>{
-//         if (e.target.matches('button')) {
-//            const key = e.target;
-//            const action = key.dataset.action;
-//            const keyContent = key.textContent;
-//            const displayNum = display.textContent;
-           
-//            // removes the pressed operator  class when second value is pressed
-//            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressedButton'));
-           
-           
-//            if (!action){
-//                if (displayNum === "0" && previousKeyType === "operator"){
-//                    display.textContent = keyContent;
-//                } else {
-//                    display.textContent = displayNum + keyContent;
-//                }
-//            }
-//            if (
-//                action === "add" ||
-//                action === "subtract" ||
-//                action === "multiply" ||
-//                action === "divide"
-//            ) {
-//                // operand button pressed is styled when clicked 
-//                key.classList.add('pressedButton');
-//                calculator.dataset.previousKeyType = 'operator';
-//            }
-//            if (action === "decimal"){
-//                display.textContent = displayNum + '.';
-//            }
-//            if (action === "clear"){
-//                display.textContent = "";
-//            }
-//            if (action === "negPositive"){
-//                console.log("Negative Positive key", key.textContent);
-//            }
-//            if (action === "calculate"){
-//                console.log("Equal key", key.textContent);
-//            }
-//         }
-//     });
-// });
-
-// function add(a,b) {
-//     return a + b;
-// }
-
-// function subtract(a,b){
-//     return a - b;
-// }
-
-// function multiply(a,b){
-//     return a * b;
-// }
-
-// function divide(a,b){
-//     return a / b;
-// }
-
-// function operate(operator, a, b){
-//     switch (operator) {
-//         case "+":
-//             add(a,b);
-//             break;
-//         case "-":
-//             subtract(a,b);
-//             break;
-//         case "*":
-//             multiply(a,b);
-//             break;
-//         case "/":
-//             divide(a,b);
-//             break;
-//         default:
-//             alert("Error: use an operator");
-//     }
-// }
 
